341fe995a5a30657644febac1d604753dd3fa3fe
hobbythai <kally.thailand@gmail.com>
2017-11-25 15:02:13 +0700

    back controll from toolbar

--- app/src/main/java/com/hobbythai/android/firebasekul/fragment/RegisterFragment.java
+++ app/src/main/java/com/hobbythai/android/firebasekul/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package com.hobbythai.android.firebasekul.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.hobbythai.android.firebasekul.MainActivity;
 import com.hobbythai.android.firebasekul.R;
 
 /**
@@ -14,6 +16,39 @@ import com.hobbythai.android.firebasekul.R;
  */
 
 public class RegisterFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //create toolbar
+        createToolbar();
+
+    }
+
+    private void createToolbar() {
+
+        //config toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        //setup title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.sub_register));
+
+        //back icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager()
+                        .popBackStack();
+            }
+        });
+
+    }
 
     @Nullable
     @Override
