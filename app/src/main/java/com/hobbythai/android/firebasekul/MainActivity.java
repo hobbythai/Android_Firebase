1a334c57ed3f7886d7f05548e98deb5b669ccbc9
hobbythai <kally.thailand@gmail.com>
2017-11-25 11:58:27 +0700

    design ui main ok

--- app/src/main/java/com/hobbythai/android/firebasekul/MainActivity.java
+++ app/src/main/java/com/hobbythai/android/firebasekul/MainActivity.java
@@ -3,11 +3,22 @@ package com.hobbythai.android.firebasekul;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.hobbythai.android.firebasekul.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            //add fragment to activity
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    }//on create
+
+}//main class
